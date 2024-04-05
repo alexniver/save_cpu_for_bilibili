591aeca082177b1d2d403e44952aec4b75b873f8
Alexniver <zhulin10541@gmail.com>
2024-04-05 21:56:39 +0800

    remove ad

--- saveCPU.js
+++ saveCPU.js
@@ -8,14 +8,39 @@ window.addEventListener('load', () => {
     window.scrollBy(0, -window.innerHeight);
   }, 5);
 
-  // 查找所有具有特定类名的元素
+  // 移除 画廊
   const elements = document.getElementsByClassName('recommended-swipe grid-anchor');
-
-  // 将HTMLCollection转换为数组，以便使用forEach方法
   const elementsArray = Array.from(elements);
-
-  // 遍历数组，移除每个元素
   elementsArray.forEach(element => {
     element.remove();
   });
+
+  // 移除广告
+  removeNthParentNodesWithClass("bili-video-card__info--ad", 6);
 });
+
+// 找到一个子元素的第七个父节点并删除它
+function removeNthParentOfChild(childElement, n) {
+  let parent = childElement.parentElement;
+  for (let i = 0; i < n; i++) {
+    if (parent === null) {
+      return; // 如果父节点不存在，直接返回
+    }
+    parent = parent.parentElement; // 循环找到第七个父节点
+  }
+  if (parent !== null) {
+    parent.remove(); // 删除第七个父节点
+  }
+}
+
+
+// 查找所有具有特定class的子元素，并删除其父节点
+function removeNthParentNodesWithClass(className, n) {
+  const elements = document.querySelectorAll('.' + className); // 查找所有具有特定class的元素
+  elements.forEach(element => {
+    // 删除父节点
+    removeNthParentOfChild(element, n);
+  });
+}
+
+
