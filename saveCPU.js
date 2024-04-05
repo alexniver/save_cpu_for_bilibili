88c0b68cbd194229c4a7e324bc3a37639f7b25dc
Alexniver <zhulin10541@gmail.com>
2024-04-05 19:57:58 +0800

    remove swap

--- saveCPU.js
+++ saveCPU.js
@@ -7,4 +7,15 @@ window.addEventListener('load', () => {
   setTimeout(() => {
     window.scrollBy(0, -window.innerHeight);
   }, 5);
+
+  // 查找所有具有特定类名的元素
+  const elements = document.getElementsByClassName('recommended-swipe-body');
+
+  // 将HTMLCollection转换为数组，以便使用forEach方法
+  const elementsArray = Array.from(elements);
+
+  // 遍历数组，移除每个元素
+  elementsArray.forEach(element => {
+    element.remove();
+  });
 });
